ac50b77eca9a4e12dc185bad2266e54765c6d183
xbrain1999 <90081185+xbrain1999@users.noreply.github.com>
2024-07-12 10:57:20 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,8 +24,8 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'metawenbo', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '卖过汽水,卷过互联网,芝士分享', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Inspire Journey with Metawenbo', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://metawenbo.xyz', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'meta,notion,e-commerce,knowledge,wisdom,mental,learn,earn,invest', // 网站关键词 英文逗号隔开
 
