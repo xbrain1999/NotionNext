47799f70c4f777ac6380b0610e34b15f26467541
xbrain1999 <90081185+xbrain1999@users.noreply.github.com>
2024-07-12 13:24:55 +0800

    Update style.js

--- themes/example/style.js
+++ themes/example/style.js
@@ -6,11 +6,27 @@
  */
 const Style = () => {
   return <style jsx global>{`
-    // 底色
-    .dark body{
-        background-color: black;
+    /* 底色 */
+    .dark body {
+      background-color: black;
     }
 
+    /* 调整页头高度和填充 */
+    .header {
+      height: 60px; /* 根据需要调整高度 */
+      padding: 10px 20px; /* 根据需要调整填充 */
+    }
+
+    /* 调整标题栏间距和填充 */
+    .title-bar {
+      margin: 20px 0; /* 根据需要调整间距 */
+      padding: 10px 0; /* 根据需要调整填充 */
+    }
+
+    /* 调整主内容的间距 */
+    #container-wrapper {
+      padding: 4px 16px; /* 根据需要调整内部填充 */
+    }
   `}</style>
 }
 
